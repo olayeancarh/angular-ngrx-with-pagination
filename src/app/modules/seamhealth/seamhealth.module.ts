fdbccd85cc08fa17d52d8ae15e33be09fe779c2e
Olayinka Akeju <akejuolayinka016@gmail.com>
2023-04-01 06:14:27 +0100

    Initial commit

---
+++ src/app/modules/seamhealth/seamhealth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SeamhealthRoutingModule } from './seamhealth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SeamhealthRoutingModule
+  ]
+})
+export class SeamhealthModule { }
