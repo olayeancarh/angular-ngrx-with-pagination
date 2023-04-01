7ef0f6aafdf8adb244cf9ca14f7427707b397a11
Olayinka Akeju <akejuolayinka016@gmail.com>
2023-04-01 08:05:08 +0100

    Created starter component for posts

---
+++ src/app/modules/seamhealth/post-table/post-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { PostTableDataSource, PostTableItem } from './post-table-datasource';
+
+@Component({
+  selector: 'app-post-table',
+  templateUrl: './post-table.component.html',
+  styleUrls: ['./post-table.component.css']
+})
+export class PostTableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<PostTableItem>;
+  dataSource: PostTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new PostTableDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
--- src/app/modules/seamhealth/seamhealth.module.ts
+++ src/app/modules/seamhealth/seamhealth.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { SeamhealthRoutingModule } from './seamhealth-routing.module';
 import { SeamhealthComponent } from './seamhealth.component';
+import { PostTableComponent } from './post-table/post-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    SeamhealthComponent
+    SeamhealthComponent,
+    PostTableComponent
   ],
   imports: [
     CommonModule,
-    SeamhealthRoutingModule
+    SeamhealthRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class SeamhealthModule { }
